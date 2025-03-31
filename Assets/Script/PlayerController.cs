1675b53ccc874628ac40b90aeca4532f7aea7543
Venus_Rosemary <2371495710@qq.com>
2025-03-31 09:43:08 +0800

    no message

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -11,6 +12,10 @@ public class PlayerController : MonoBehaviour
     public GameObject VFXSlowDown;
     public GameObject PlayerHitVFX;
     public GameObject triggerVFX;
+    public GameObject scoreTextPrefab; // 3D分数文本预制体
+    public float scoreTextDuration = 1.5f; // 分数文本显示时间
+    public float scoreTextFloatSpeed = 1.0f; // 分数文本上浮速度
+    
     private bool isDisabled = false;//处于被石头减速状态
     private float disableTimer = 0f;
     private Vector3 originalScale; // 保存原始缩放
@@ -128,12 +133,23 @@ public class PlayerController : MonoBehaviour
                 }
             }
             triggerVFX.SetActive(true);
-            ScoreController.Instance.Score(other.GetComponent<FruitSettings>().score);
+            SoundManagement.Instance.PlayerNumberTwoSFX(0);
+
+            // 获取水果分数
+            int fruitScore = other.GetComponent<FruitSettings>().score;
+            
+            // 显示3D分数文本
+            ShowScoreText(fruitScore);
+            
+            // 添加分数
+            ScoreController.Instance.Score(fruitScore);
+            
             Destroy(other.gameObject);
         }
         else if (other.CompareTag("Stone"))
         {
             triggerVFX.SetActive(true);
+            SoundManagement.Instance.PlaySFX(1);
             Destroy(other.gameObject);
             DisableControl(3f);
         }
@@ -234,4 +250,77 @@ public class PlayerController : MonoBehaviour
         return isFruitsHidden;
     }
     #endregion
+
+
+    // 显示3D分数文本
+    void ShowScoreText(int score)
+    {
+        if (scoreTextPrefab != null)
+        {
+            // 确定生成位置
+            Vector3 spawnPosition;
+            // 如果没有指定生成点，就在玩家头顶上方生成
+            spawnPosition = transform.position + Vector3.up * 2f;
+
+            // 实例化分数文本
+            GameObject scoreTextObj = Instantiate(scoreTextPrefab, spawnPosition, Quaternion.identity);
+
+            // 设置文本内容
+            TMP_Text textMesh = scoreTextObj.GetComponentInChildren<TMP_Text>();
+            if (textMesh != null)
+            {
+                textMesh.text = "+" + score.ToString();
+            }
+
+            // 启动协程控制文本动画和销毁
+            StartCoroutine(AnimateScoreText(scoreTextObj));
+        }
+    }
+
+    // 控制分数文本动画和销毁
+    IEnumerator AnimateScoreText(GameObject scoreTextObj)
+    {
+        float timer = 0f;
+        Vector3 startPosition = scoreTextObj.transform.position;
+        Vector3 endPosition = startPosition + Vector3.up * 1.5f;
+
+        // 淡入
+        CanvasGroup canvasGroup = scoreTextObj.GetComponent<CanvasGroup>();
+        if (canvasGroup != null)
+        {
+            canvasGroup.alpha = 0f;
+            float fadeInTime = 0.2f;
+            while (timer < fadeInTime)
+            {
+                canvasGroup.alpha = timer / fadeInTime;
+                timer += Time.deltaTime;
+                yield return null;
+            }
+            canvasGroup.alpha = 1f;
+            timer = 0f;
+        }
+
+        // 上浮动画
+        while (timer < scoreTextDuration)
+        {
+            float t = timer / scoreTextDuration;
+            scoreTextObj.transform.position = Vector3.Lerp(startPosition, endPosition, t);
+
+            // 如果有CanvasGroup组件，在结束前开始淡出
+            if (canvasGroup != null && timer > scoreTextDuration * 0.7f)
+            {
+                float fadeOutProgress = (timer - scoreTextDuration * 0.7f) / (scoreTextDuration * 0.3f);
+                canvasGroup.alpha = 1f - fadeOutProgress;
+            }
+
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        // 销毁分数文本对象
+        Destroy(scoreTextObj);
+    }
 }
+
+
+
